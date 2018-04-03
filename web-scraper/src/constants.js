11c5c27ee61d5075fd8dbe5db1b27a5ea5f8a4c3
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-03 10:15:27 +1000

    Add competition url

--- web-scraper/src/constants.js
+++ web-scraper/src/constants.js
@@ -1,12 +1,27 @@
 import querystring from 'querystring';
 
-const URL = 'https://sportfix.net/app/teamdetails.aspx';
+const COMPETITION_ROUTE = 'competition.aspx';
+const DIVISION_ID = '806';
+const FIXTURE_ROUTE = 'teamdetails.aspx';
+const SEASON_ID = '3196';
 const SPORT_FIX_ID = '186ff05f-201d-46ea-bc7b-85dc3ba3fe70';
+const SPORT_ID = '329';
 const TEAM_ID = '91428';
+const URL = 'https://sportfix.net/app/';
 
-const opts = {
+const fixtureOpts = {
   sportFixId: SPORT_FIX_ID,
   teamId: TEAM_ID
 };
 
-export const FIXTURE_URL = URL + '?' + querystring.encode(opts);
+const competitionOpts = {
+  div: DIVISION_ID,
+  sea: SEASON_ID,
+  sp: SPORT_ID
+};
+
+export const FIXTURE_URL =
+  URL + FIXTURE_ROUTE + '?' + querystring.encode(fixtureOpts);
+
+export const COMPETITION_URL =
+  URL + COMPETITION_ROUTE + '?' + querystring.encode(ladderOpts);
