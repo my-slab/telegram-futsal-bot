93fa110b53624c92129dd77e44d8e9252748a4e0
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-03 11:57:02 +1000

    rm lib

--- web-scraper/lib/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const API_TOKEN = exports.API_TOKEN = '429243168:AAHJQX2W7RdqtLKg5A4bN6O5Jgb53zApYwU';
--- web-scraper/lib/scraper/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _index = require('../index');
-
-test('adds 1 + 2 to equal 3', async () => {
-  await (0, _index.setup)();
-});
