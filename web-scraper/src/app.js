affc171ef36afcc7741fc6122412390d46f60e00
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-04 10:22:30 +1000

    ♻️ Group all bot logic

--- web-scraper/src/app.js
+++ web-scraper/src/app.js
@@ -1,33 +1,5 @@
-import { getTable, getNextMatch } from './scraper';
-import bot, { getUsername } from './bot';
+import bot, { setup } from './bot';
 
-const init = async () => {
-  bot.options.username = await getUsername(bot);
-  console.log(`Server has initialized bot: ${bot.options.username}`);
-
-  bot.command('fixture', async ctx => {
-    ctx.reply('📅');
-    try {
-      const result = await getNextMatch();
-      return ctx.reply(result);
-    } catch (e) {
-      console.log(e);
-      ctx.reply('️⛔️ Something went wrong');
-    }
-  });
-
-  bot.command('table', async ctx => {
-    ctx.reply('🔝');
-    try {
-      const result = await getTable();
-      return ctx.replyWithMarkdown(result);
-    } catch (e) {
-      console.log(e);
-      ctx.reply('⛔️ Something went wrong');
-    }
-  });
-
-  bot.startPolling();
-};
-
-init();
+(async () => {
+  await setup(bot);
+})();
--- web-scraper/src/bot.js
+++ web-scraper/src/bot.js
@@ -1,11 +1,42 @@
 import Telegraf from 'telegraf';
 
+import { getNextMatch, getTable } from './scraper';
+
 const API_TOKEN = process.env.TELEGRAM_API_TOKEN || '';
 const bot = new Telegraf(API_TOKEN);
 
-export const getUsername = async ({ telegram }) => {
+const getUsername = async ({ telegram }) => {
   const { username } = await telegram.getMe();
   return username;
 };
+
+const commandFixture = async ctx => {
+  ctx.reply('📅');
+  try {
+    const result = await getNextMatch();
+    return ctx.reply(result);
+  } catch (e) {
+    console.log(e);
+    ctx.reply('️⛔️ Something went wrong');
+  }
+};
+
+const commandTable = async ctx => {
+  ctx.reply('🔝');
+  try {
+    const result = await getTable();
+    return ctx.replyWithMarkdown(result);
+  } catch (e) {
+    console.log(e);
+    ctx.reply('⛔️ Something went wrong');
+  }
+});
+
+export const setup = async (bot) => {
+  bot.options.username = await getUsername(bot);
+  bot.command('fixture', commandFixture);
+  bot.command('table', commandTable);
+  bot.startPolling();
+}
 
 export default bot;
