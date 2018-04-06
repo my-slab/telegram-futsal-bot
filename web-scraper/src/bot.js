2678d76c2f195b27c067b0d06112023162f6c1d4
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-07 01:56:22 +1000

    Add button for availability

--- web-scraper/src/bot.js
+++ web-scraper/src/bot.js
@@ -1,4 +1,6 @@
 import Telegraf from 'telegraf';
+import Extra from 'telegraf/extra';
+import Markup from 'telegraf/markup';
 
 import { getNextMatch, getTable } from './scraper';
 
@@ -8,6 +10,23 @@ const bot = new Telegraf(API_TOKEN);
 const getUsername = async ({ telegram }) => {
   const { username } = await telegram.getMe();
   return username;
+};
+
+const commandAvailable = async ctx => {
+  try {
+    return ctx.reply(
+      '<b>In 👍</b> or <b>Out 👎</b>',
+      Extra.HTML().markup(m =>
+        m.inlineKeyboard([
+          m.callbackButton('In', 'In'),
+          m.callbackButton('Out', 'Out')
+        ])
+      )
+    );
+  } catch (e) {
+    console.log(e);
+    ctx.reply('⛔️ Something went wrong');
+  }
 };
 
 const commandFixture = async ctx => {
@@ -30,13 +49,20 @@ const commandTable = async ctx => {
     console.log(e);
     ctx.reply('⛔️ Something went wrong');
   }
-});
+};
 
-export const setup = async (bot) => {
+const onCallback = async ctx => {
+  const { data, from: { username } } = ctx.callbackQuery;
+  ctx.answerCbQuery(`${username}: ${data}`);
+};
+
+export const setup = async bot => {
   bot.options.username = await getUsername(bot);
+  bot.command('available', commandAvailable);
   bot.command('fixture', commandFixture);
   bot.command('table', commandTable);
+  bot.on('callback_query', onCallback);
   bot.startPolling();
-}
+};
 
 export default bot;
