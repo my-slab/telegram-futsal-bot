6222feb0fd2cab2d852105f9094a6654be990d06
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-04-08 22:44:37 +1000

    Add api token to constants

--- web-scraper/src/constants.js
+++ web-scraper/src/constants.js
@@ -1,5 +1,6 @@
 import querystring from 'querystring';
 
+const BASE_URL = 'https://sportfix.net/app/';
 const COMPETITION_ROUTE = 'competition.aspx';
 const DIVISION_ID = '806';
 const FIXTURE_ROUTE = 'teamdetails.aspx';
@@ -7,7 +8,6 @@ const SEASON_ID = '3196';
 const SPORT_FIX_ID = '186ff05f-201d-46ea-bc7b-85dc3ba3fe70';
 const SPORT_ID = '329';
 const TEAM_ID = '91428';
-const URL = 'https://sportfix.net/app/';
 
 const fixtureOpts = {
   sportFixId: SPORT_FIX_ID,
@@ -21,8 +21,7 @@ const competitionOpts = {
   sportFixId: SPORT_FIX_ID
 };
 
-const buildUrl = (url, opts) =>
-  URL + COMPETITION_ROUTE + '?' + querystring.encode(opts);
+const buildUrl = (url, opts) => BASE_URL + url + '?' + querystring.encode(opts);
 
 export const callbackButtons = {
   IN: 'in',
@@ -33,3 +32,5 @@ export const callbackButtons = {
 export const COMPETITION_URL = buildUrl(COMPETITION_ROUTE, competitionOpts);
 
 export const FIXTURE_URL = buildUrl(FIXTURE_ROUTE, fixtureOpts);
+
+export const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN || '';
