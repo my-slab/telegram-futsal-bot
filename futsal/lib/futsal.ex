a4e7cd4c29759c739575a93e6b8c5805e03ed0be
Mitchell Stewart <mitchell.stewart@mylsb.email>
2018-05-03 00:58:42 +1000

    Move .gitignore

---
+++ futsal/lib/futsal.ex
@@ -0,0 +1,18 @@
+defmodule Futsal do
+  @moduledoc """
+  Documentation for Futsal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Futsal.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
